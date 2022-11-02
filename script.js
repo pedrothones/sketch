4cd8fea73cfbfc76054a0bbc7d40c2cfb43d87d2
pedrothones <pedro.thones@gmail.com>
2022-11-01 21:43:07 -0300

    Added the squares, function to make drawing work and reset board button

---
+++ script.js
@@ -0,0 +1,34 @@
+const board = document.querySelector(".board");
+let boxSize = 600;
+let measures = boxSize / 16;
+let isDrawing = false;
+
+for (i = 0; i < 256; i++) {
+  let square = document.createElement("div");
+  // square.style.backgroundColor = "white";
+  square.style.height = `${measures}px`;
+  square.style.width = `${measures}px`;
+  square.classList.add("square");
+  board.insertAdjacentElement("beforeend", square);
+}
+const resetButton = document.querySelector(".reset");
+const paint = document.querySelectorAll(".square");
+
+//function to be executed when drawing is true
+
+paint.forEach((item) => {
+  //reset the board
+  resetButton.addEventListener(
+    "click",
+    () => (item.style.backgroundColor = "wheat")
+  );
+  item.addEventListener("mousedown", () => (isDrawing = true));
+  item.addEventListener("mouseup", () => (isDrawing = false));
+  item.addEventListener("mousemove", () => {
+    if (!isDrawing) return;
+    item.style.backgroundColor = "black";
+  });
+  // item.addEventListener("mousedown", () => {
+  //   item.style.backgroundColor = "black";
+  // });
+});
