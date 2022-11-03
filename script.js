6a0bfa05d8171e07f0776a6bf88276265c551e6c
pedrothones <pedro.thones@gmail.com>
2022-11-02 21:33:08 -0300

    Added a button to create x amount of squares in the board

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
 const board = document.querySelector(".board");
+const smallBoard = document.querySelector(".thirty-two");
+const bigBoard = document.querySelector(".sixty-four");
 let boxSize = 600;
-let boardSquares = 64;
+let boardSquares = 8;
 let measures = boxSize / boardSquares;
 let isDrawing = false;
 
-for (i = 0; i < boardSquares * boardSquares; i++) {
-  let square = document.createElement("div");
-  // square.style.backgroundColor = "white";
-  square.style.height = `${measures}px`;
-  square.style.width = `${measures}px`;
-  square.classList.add("square");
-  board.insertAdjacentElement("beforeend", square);
-}
-const resetButton = document.querySelector(".reset");
-const paint = document.querySelectorAll(".square");
+function createDivs() {
+  for (i = 0; i < boardSquares * boardSquares; i++) {
+    let square = document.createElement("div");
+    // square.style.backgroundColor = "white";
+    square.style.height = `${measures}px`;
+    square.style.width = `${measures}px`;
+    square.classList.add("square");
+    board.insertAdjacentElement("beforeend", square);
+  }
 
-//function to be executed when drawing is true
+  const resetButton = document.querySelector(".reset");
+  const paint = document.querySelectorAll(".square");
 
-paint.forEach((item) => {
-  //reset the board
-  resetButton.addEventListener(
-    "click",
-    () => (item.style.backgroundColor = "wheat")
-  );
-  item.addEventListener("mousedown", () => (isDrawing = true));
-  item.addEventListener("mouseup", () => (isDrawing = false));
-  item.addEventListener("mousemove", () => {
-    if (!isDrawing) return;
-    item.style.backgroundColor = "black";
+  //function to be executed when drawing is true
+  paint.forEach((item) => {
+    //RESET THE COLOR
+    resetButton.addEventListener(
+      "click",
+      () => (item.style.backgroundColor = "wheat")
+    );
+
+    item.addEventListener("mousedown", () => (isDrawing = true));
+    item.addEventListener("mouseup", () => (isDrawing = false));
+    item.addEventListener("mousemove", () => {
+      if (!isDrawing) return;
+      item.style.backgroundColor = "black";
+    });
   });
-  // item.addEventListener("mousedown", () => {
-  //   item.style.backgroundColor = "black";
-  // });
-});
+}
+function removeDivs() {
+  const paint = document.querySelectorAll(".square");
+  if (paint.length > 0) {
+    console.log(paint.length);
+    paint.forEach((item) => item.remove());
+  } else return;
+}
+
+smallBoard.onclick = function () {
+  removeDivs();
+  createDivs();
+};
+bigBoard.onclick = function () {
+  removeDivs();
+  createDivs();
+};
