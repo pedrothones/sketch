f3ec98cc61814ab5faea6f1d7068312d704d2d3f
pedrothones <pedro.thones@gmail.com>
2022-11-06 18:00:17 -0300

    Added erasor and color selector buttons

--- script.js
+++ script.js
@@ -2,55 +2,74 @@ const board = document.querySelector(".board");
 const smallBoard = document.querySelector(".thirty-two");
 const bigBoard = document.querySelector(".sixty-four");
 const resetButton = document.querySelector(".reset");
+const colorInput = document.querySelector(".color");
+const erasor = document.querySelector(".erasor");
+const black = document.querySelector(".black");
 let boxSize = 600;
 let boardSquares = 32;
 let measures = boxSize / boardSquares;
 let isDrawing = false;
+let color = "black";
 
 function createDivs() {
-  console.log(boardSquares);
   for (i = 0; i < boardSquares * boardSquares; i++) {
     let square = document.createElement("div");
-    // square.style.backgroundColor = "white";
     square.style.height = `${measures}px`;
     square.style.width = `${measures}px`;
     square.classList.add("square");
     board.insertAdjacentElement("beforeend", square);
   }
-
-  const paint = document.querySelectorAll(".square");
-
-  //function to be executed when drawing is true
-  paint.forEach((item) => {
+}
+function draw() {
+  document.querySelectorAll(".square").forEach((item) => {
     item.addEventListener("mousedown", () => (isDrawing = true));
     item.addEventListener("mouseup", () => (isDrawing = false));
     item.addEventListener("mousemove", () => {
       if (!isDrawing) return;
-      item.style.backgroundColor = "black";
+      item.style.backgroundColor = color;
     });
   });
 }
+
 function removeDivs() {
   const paint = document.querySelectorAll(".square");
   if (paint.length > 0) {
     paint.forEach((item) => item.remove());
   } else return;
 }
-createDivs();
 
 smallBoard.onclick = function () {
   boardSquares = 32;
   measures = boxSize / boardSquares;
   removeDivs();
   createDivs();
+  draw();
 };
 bigBoard.onclick = function () {
   boardSquares = 64;
   measures = boxSize / boardSquares;
   removeDivs();
   createDivs();
+  draw();
 };
 resetButton.onclick = function () {
   removeDivs();
   createDivs();
+  draw();
 };
+erasor.onclick = function () {
+  color = "wheat";
+};
+black.onclick = function () {
+  color = "black";
+};
+
+function changeColor() {
+  colorInput.addEventListener("change", () => {
+    color = colorInput.value;
+  });
+}
+console.log(color);
+changeColor();
+createDivs();
+draw();
